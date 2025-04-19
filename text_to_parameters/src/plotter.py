f6a12d8c7127b26eb455f46b4c7390d59114c1ea
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2025-04-19 08:53:18 +0000

    plotter.py

---
+++ text_to_parameters/src/plotter.py
@@ -0,0 +1,14 @@
+# import matplotlib.pyplot as plt
+
+# from shapely.geometry import Polygon, LineString
+# from typing import List
+
+# class Plotter:
+#     @staticmethod
+#     def plot(
+#         polygon: Polygon,
+#         core_polygons: List[Polygon],
+#         segments: List[LineString],
+#         selected_indices: List[int],
+#     ):
+#         return
