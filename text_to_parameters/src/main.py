e72cc60b9facfd02951140f241e9479e4cccaaa7
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2025-04-21 10:47:11 +0000

    main.py

---
+++ text_to_parameters/src/main.py
@@ -0,0 +1,36 @@
+import os
+
+from agents import Agent, Runner
+
+from plotter import Plotter
+from testsets import TestSets
+from selector import SegmentSelector, SegmentSelectorConfiguration, SegmentSelectorOutput
+
+
+if __name__ == "__main__":
+    agent = Agent(
+        name=SegmentSelectorConfiguration.NAME,
+        model=SegmentSelectorConfiguration.MODEL_NAME,
+        model_settings=SegmentSelectorConfiguration.MODEL_SETTINGS,
+        instructions=SegmentSelectorConfiguration.INSTRUCTIONS,
+        output_type=SegmentSelectorOutput,
+        tools=SegmentSelector.tools(),
+    )
+
+    runs = os.path.abspath(os.path.join(__file__, "../../runs"))
+
+    os.makedirs(runs, exist_ok=True)
+
+    for testcase in TestSets():
+        for rqi, request in enumerate(testcase.TEXTS):
+            response = Runner.run_sync(agent, request)
+            print(response.final_output.__dict__)
+
+            fig = Plotter.plot(
+                testcase.BOUNDARY,
+                SegmentSelector.explode_to_segments(testcase.BOUNDARY),
+                response.final_output,
+                title=f"{request[:request.index(':')]}",
+            )
+
+            fig.savefig(os.path.join(runs, f"{testcase.__name__}_{rqi}.png"))
